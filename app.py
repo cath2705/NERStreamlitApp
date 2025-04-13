3c683cbf1a30e986869417ea36f2ceec8b627842
cath2705 <cparis@nd.edu>
2025-04-13 19:54:38 -0400

    updating text on my homepage

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="NER Streamlit App", layout="wide")
 
-st.markdown("# 👋 Welcome to the Custom NER App")
+st.markdown("# 👋 Welcome to My Custom NER App")
+st.markdown("NER refers to Named Entity Recognition. Read more to understand what exactly this means! ")
 st.markdown("Use the sidebar to navigate through the different tools and learn about Named Entity Recognition.")
